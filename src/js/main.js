1203afca3bd5c43ae6a1b9ce4ef114851ffb7874
ianjburgess <ianjbur@gmail.com>
2016-06-23 13:00:46 +1000

    minor nav fixes

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,18 @@ $('#mainNav').on('show.bs.collapse', function() {
 $('#mainNav').on('hide.bs.collapse', function() {
     $('.nav-pills').removeClass('nav-stacked');
 });
+
+//Remove nav stacked class on screen > resize
+(function($) {
+    var $window = $(window),
+        $stacked = $('.nav-pills');
+        $collapse = $('#mainNav');
+
+    $window.resize(function resize(){
+        if ($window.width() > 767) {
+            $collapse.removeClass('in');
+            $stacked.removeClass('nav-stacked');
+
+        }
+    }).trigger('resize');
+})(jQuery);
