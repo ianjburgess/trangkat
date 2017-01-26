1ff865ab884c9f23b3e3993739c658bf317bcf40
ianjburgess <ianjbur@gmail.com>
2017-01-27 10:36:13 +1100

    added faqs pages

--- js/main.js
+++ js/main.js
@@ -1,33 +1,4 @@
-//Stack menu when collapsed
-
-//$('#mainNav').on('show.bs.collapse', function() {
-//    $('.nav-pills').addClass('nav-stacked');
-//});
-
-//Unstack menu when not collapsed
-//$('#mainNav').on('hide.bs.collapse', function() {
-//    $('.nav-pills').removeClass('nav-stacked');
-//});
-
-//Remove nav stacked class on screen > resize
-(function($) {
-    var $window = $(window),
-        $stacked = $('.nav-pills');
-        $collapse = $('#mainNav');
-
-    $window.resize(function resize(){
-        if ($window.width() > 767) {
-            $collapse.removeClass('in');
-            $stacked.removeClass('nav-stacked');
-
-        }
-    }).trigger('resize');
-})(jQuery);
-
 $(document).ready(function(){
-
-$(function(){
- 
     $(document).on( 'scroll', function(){
  
     	if ($(window).scrollTop() > 100) {
@@ -38,15 +9,13 @@ $(function(){
 	});
  
 	$('.scroll-top-wrapper').on('click', scrollToTop);
-});
- 
-function scrollToTop() {
-	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
-	element = $('body');
-	offset = element.offset();
-	offsetTop = offset.top;
-	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
-}
 
+    function scrollToTop() {
+        verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+        element = $('body');
+        offset = element.offset();
+        offsetTop = offset.top;
+        $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+    }
 });
 
