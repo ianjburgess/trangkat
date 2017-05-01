1665c7fb6afa5523fae749d71e4e856493f9f377
ianjburgess <ianjbur@gmail.com>
2017-05-01 18:09:01 +1000

    wp-start

---
+++ wp-theme/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+    <div class="text-center">© Copyright Trang Kat Photography 2017 | <a href="pages/terms&conditions.html">Terms & Conditions</a> | newborn photography melbourne</div>
+</div>
