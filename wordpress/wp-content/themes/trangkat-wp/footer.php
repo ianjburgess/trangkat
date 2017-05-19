8f42f3d6b0a9f0d11398ccbf2d2bfa75cec2e9dc
ianjburgess <ianjbur@gmail.com>
2017-05-19 16:54:53 +1000

    fb plugin

--- wordpress/wp-content/themes/trangkat-wp/footer.php
+++ wordpress/wp-content/themes/trangkat-wp/footer.php
@@ -1,3 +1,12 @@
+<div id="fb-root"></div>
+        <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
 <div class="text-center fb-box">
     <div class="fb-page" data-href="https://www.facebook.com/TrangKatphoto/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/TrangKatphoto/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/TrangKatphoto/">TrangKat photography</a></blockquote></div>
 </div>
