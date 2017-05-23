672262ce6ca2fc41dd5d94c8dbbe4beb8ce75dd7
ianjburgess <ianjbur@gmail.com>
2017-05-23 12:06:01 +1000

    na

--- wordpress/wp-content/themes/trangkat-wp/sidebar.php
+++ wordpress/wp-content/themes/trangkat-wp/sidebar.php
@@ -1,1 +1,13 @@
-<?php wp_sidebar(); ?>
+<?php
+/**
+ * The sidebar for our theme.
+ *
+ * 
+ *
+ * @package Trang Kat Photography
+ */
+?>
+
+<?php if ( is_active_sidebar( 'custom-side-bar' ) ) : ?>
+    <?php dynamic_sidebar( 'custom-side-bar' ); ?>
+<?php endif; ?>
